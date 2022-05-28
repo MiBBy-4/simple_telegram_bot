6a5e8e86ba90fd8ab8f16486f1ce4b128c071d4b
MiBBy-4 <maksim.762373maks@bk.ru>
2022-05-28 22:13:36 +0300

    feat: connect application with dev database

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+database_connection_file = File.join(Rails.root, 'database_connection.env')
+api_token = File.join(Rails.root, 'api_token.env')
+load(database_connection_file) if File.exist?(database_connection_file)
+load(api_token) if File.exist?(api_token)
 # Initialize the Rails application.
 Rails.application.initialize!
